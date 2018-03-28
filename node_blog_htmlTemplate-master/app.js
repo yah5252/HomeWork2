755a2441e81bc2b4d08602316ed2ed37253d79ac
yah5252 <30340879+yah5252@users.noreply.github.com>
2018-03-28 23:12:51 +0800

    delete

--- node_blog_htmlTemplate-master/app.js
+++ node_blog_htmlTemplate-master/app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var engine = require('ejs-locals');
+app.engine('ejs',engine);
+app.set('views','./views');
+app.set('view engine','ejs');
 
 var index = require('./routes/index');
 
--- node_blog_htmlTemplate-master/routes/index.js
+++ node_blog_htmlTemplate-master/routes/index.js
@@ -3,14 +3,22 @@ var router = express.Router();
 
 
 router.get('/', function(req, res,next) {
-    res.render('index',{title:'Express'});
+    res.render('../index',{title:'Express'});
 });
 
-router.get('/post', function(req, res,next) {
-    res.render('post',{title:'Express'});
+router.get('/', function(req, res,next) {
+    res.render('../post',{title:'Express'});
 });
 
 router.get('/dashboard/article', function(req, res,next) {
+    res.render('dashboard/article',{title:'Express'});
+});
+
+router.get('/dashboard/categories', function(req, res,next) {
+    res.render('dashboard/article',{title:'Express'});
+});
+
+router.get('/dashboard/signup', function(req, res,next) {
     res.render('dashboard/article',{title:'Express'});
 });
 
